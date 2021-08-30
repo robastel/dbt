ca8d2676e9131851c3e2331f1ceacb0d4d401d33
robastel <rob.astel@gmail.com>
2021-08-30 18:51:11 -0400

    Add draft_pick_id primary key and tests
    Signed-off-by: Robert Astel <robert.astel@dashlane.com>

--- models/staging/fantasy_football/espn/staging__espn_draft_picks.sql
+++ models/staging/fantasy_football/espn/staging__espn_draft_picks.sql
@@ -1,9 +1,45 @@
+WITH picks_without_primary_key AS
+(
+    SELECT
+        platform
+        , platform_season_id
+        , platform_draft_id
+        , platform_team_id
+        , round_num
+        , round_pick_num
+        , CASE
+            WHEN MOD(round_num, 2) = 1 THEN round_pick_num
+            ELSE
+                MAX(round_pick_num) OVER (
+                    PARTITION BY platform, platform_season_id, platform_draft_id
+                ) + 1 - round_pick_num
+          END AS draft_slot_num
+        , (round_num - 1)
+            * MAX(round_pick_num) OVER (
+                PARTITION BY platform, platform_season_id, platform_draft_id
+              )
+            + round_pick_num AS overall_pick_num
+        , player_id
+        , player_name
+        , player_position
+        , player_team
+        , player_years_experience
+        , is_keeper
+    FROM
+        {{ ref('base__espn_draft_picks') }}
+)
+
 SELECT
-    {{ 
+    {{
+        dbt_utils.surrogate_key(
+            ['platform', 'platform_season_id', 'platform_draft_id', 'overall_pick_num']
+        )
+    }} AS draft_pick_id
+    , {{
         dbt_utils.surrogate_key(
             ['platform', 'platform_season_id', 'platform_draft_id']
-        ) 
-    }} AS draft_id
+        )
+      }} AS draft_id
     , *
 FROM
-    {{ ref('base__espn_draft_picks') }}
+    picks_without_primary_key
--- models/staging/fantasy_football/sleeper/staging__sleeper_draft_picks.sql
+++ models/staging/fantasy_football/sleeper/staging__sleeper_draft_picks.sql
@@ -1,9 +1,33 @@
 SELECT
-    {{ 
+    {{
+        dbt_utils.surrogate_key(
+            ['platform', 'platform_season_id', 'platform_draft_id', 'overall_pick_num']
+        )
+    }} AS draft_pick_id
+    , {{
         dbt_utils.surrogate_key(
             ['platform', 'platform_season_id', 'platform_draft_id']
         ) 
-    }} AS draft_id
-    , *
+      }} AS draft_id
+    , platform
+    , platform_season_id
+    , platform_draft_id
+    , platform_team_id
+    , round_num
+    , CASE
+        WHEN MOD(round_num, 2) = 1 THEN draft_slot_num
+        ELSE
+            COUNT(platform_team_id) OVER (
+                PARTITION BY platform, platform_season_id, platform_draft_id, round_num
+            ) + 1 - draft_slot_num
+      END AS round_pick_num
+    , draft_slot_num
+    , overall_pick_num
+    , player_id
+    , player_name
+    , player_position
+    , player_team
+    , player_years_experience
+    , is_keeper
 FROM
     {{ ref('base__sleeper_draft_picks') }}
