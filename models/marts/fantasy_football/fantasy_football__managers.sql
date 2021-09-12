c522ed9c3940795bc928c0dd79078b9308559bea
robastel <rob.astel@gmail.com>
2021-09-12 11:52:43 -0400

    Add regular season aggs to managers models
    Signed-off-by: robastel <rob.astel@gmail.com>

--- models/marts/fantasy_football/fantasy_football__managers.sql
+++ models/marts/fantasy_football/fantasy_football__managers.sql
@@ -27,11 +27,81 @@ WITH matchup_counts AS
     {{ dbt_utils.group_by(3) }}
 )
 
+, completed_regular_seasons AS
+(
+    SELECT
+        m.season_id
+        , min(is_completed) AS min_is_completed
+    FROM
+        {{ ref('fantasy_football__matchups') }} AS m
+    WHERE
+        m.is_regular_season_matchup = 1
+    {{ dbt_utils.group_by(1) }}
+    HAVING
+        min_is_completed = 1
+)
+
+, regular_season_aggs AS
+(
+    SELECT
+        m.manager_id
+        , m.season_id
+        , SUM(m.win) AS win_count
+        , SUM(m.tie) AS tie_count
+        , SUM(CASE WHEN m.is_median_matchup = 0 THEN m.points ELSE 0 END) AS season_points
+        , MAX(CASE WHEN m.is_median_matchup = 0 THEN m.points ELSE 0 END) AS single_week_most_points
+    FROM
+        completed_regular_seasons AS crs
+    JOIN
+        {{ ref('fantasy_football__matchups') }} AS m
+        ON crs.season_id = m.season_id
+    {{ dbt_utils.group_by(2) }}
+)
+
+, regular_season_records AS
+(
+    SELECT
+        rsa.manager_id
+        , rsa.season_id
+        , ROW_NUMBER() OVER (
+            PARTITION BY rsa.season_id
+            ORDER BY rsa.win_count DESC, rsa.tie_count DESC, rsa.season_points DESC
+          ) AS standing
+        , ROW_NUMBER() OVER (
+            PARTITION BY rsa.season_id
+            ORDER BY rsa.season_points DESC
+          ) AS points_standing
+        , ROW_NUMBER() OVER (
+            PARTITION BY rsa.season_id
+            ORDER BY rsa.single_week_most_points DESC
+          ) AS single_week_points_standing
+    FROM
+        regular_season_aggs AS rsa
+)
+
+, regular_season AS
+(
+    SELECT
+        rsr.manager_id
+        , COUNT(CASE WHEN rsr.standing = 1 THEN 1 END) AS regular_season_first_place_count
+        , COUNT(CASE WHEN rsr.points_standing = 1 THEN 1 END) AS regular_season_most_points_count
+        , COUNT(CASE WHEN rsr.single_week_points_standing = 1 THEN 1 END) AS regular_season_single_week_most_points_count
+    FROM
+        regular_season_records AS rsr
+    {{ dbt_utils.group_by(1) }}
+)
+
 SELECT
     c.*
     , ROUND(((1.0 * c.win_count) + (0.5 * c.tie_count)) / c.matchup_count, 3) AS win_rate
     , ROUND(((1.0 * c.regular_season_win_count) + (0.5 * c.regular_season_tie_count)) / c.regular_season_matchup_count, 3) AS regular_season_win_rate
     , ROUND(1.0 * c.playoff_win_count / c.playoff_matchup_count, 3) AS playoff_win_rate
     , ROUND(1.0 * c.made_playoffs_count / c.season_count, 3) AS made_playoffs_rate
+    , rs.regular_season_first_place_count
+    , rs.regular_season_most_points_count
+    , rs.regular_season_single_week_most_points_count
 FROM
     matchup_counts AS c
+LEFT JOIN
+    regular_season AS rs
+    ON c.manager_id = rs.manager_id
